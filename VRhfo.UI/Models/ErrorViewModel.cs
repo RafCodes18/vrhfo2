afa2d9783bfc25aa425529313e9ed6a9a91fc00c
Rafael Parra <rafael12@gmx.com>
2023-07-06 16:42:01 -0500

    Add project files.

---
+++ VRhfo.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VRhfo.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
