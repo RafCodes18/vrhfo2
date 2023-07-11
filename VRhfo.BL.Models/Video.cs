8ba080acb2df80c7d0d5ad49ada57250d343c5d6
Rafael Parra <rafael12@gmx.com>
2023-07-11 14:51:26 -0500

    Video model updated

--- VRhfo.BL.Models/Video.cs
+++ VRhfo.BL.Models/Video.cs
@@ -1,37 +1,38 @@
-﻿using VRhfo.BL.Models;
-
-public class Video
+﻿namespace VRhfo.BL.Models
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Studio { get; set; }
-    public string ThumbnailUrl { get; set; }
-    public string VideoUrl { get; set; }
-    public string Description { get; set; }
-    public string Genre { get; set; }
-    public DateTime UploadDate { get; set; }
-    public TimeSpan Duration { get; set; }
-    public List<string> Tags { get; set; }
-    public int Views { get; set; }
-    public int RatingCount { get; set; }
-    public bool IsPublic { get; set; }  //True = 5 min preview vids, false = Subscription full length
-    public bool IsPreview { get; set; }// better clarity
-
-
-    //Think IceCube, "EXPLICIT CONTENT" sold more NWA albums. Viewers of my site are degenerates, like me, adding "WARNING: STRONG HYPNOSIS", or "WARNING: STRONG HYPNOTIC CONTENT"  adds to the experience.
-    public string ContentWarning { get; set; }
-    public int Likes { get; set; }
-    public int Dislikes { get; set; }
-
-    public double AverageRating
+    public class Video
     {
-        get
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Studio { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string VideoUrl { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public DateTime UploadDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<string> Tags { get; set; }
+        public int Views { get; set; }
+        public int RatingCount { get; set; }
+        public bool IsPublic { get; set; }  //For full length vids that are free, but not previews
+        public bool IsPreview { get; set; }// If True, returns 5 min preview, if false must be paid.
+
+
+        //Think IceCube, "EXPLICIT CONTENT" sold more NWA albums. Viewers of my site are degenerates, like me, adding "WARNING: STRONG HYPNOSIS", or "WARNING: STRONG HYPNOTIC CONTENT"  adds to the experience.
+        public string ContentWarning { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+
+        public double AverageRating
         {
-            int totalVotes = Likes + Dislikes;
-            return totalVotes > 0 ? (double)Likes / totalVotes * 100 : 0;
+            get
+            {
+                int totalVotes = Likes + Dislikes;
+                return totalVotes > 0 ? (double)Likes / totalVotes * 100 : 0;
+            }
         }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
     }
-
-    public virtual ICollection<Comment> Comments { get; set; }
-
 }
