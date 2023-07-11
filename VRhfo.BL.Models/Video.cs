4235de1acc21cc94861f17043be175729bcde762
Rafael Parra <rafael12@gmx.com>
2023-07-10 23:22:24 -0500

    formatting

--- VRhfo.BL.Models/Video.cs
+++ VRhfo.BL.Models/Video.cs
@@ -7,11 +7,11 @@ public class Video
     public string Studio { get; set; }
     public string ThumbnailUrl { get; set; }
     public string VideoUrl { get; set; }
-    public DateTime UploadDate { get; set; }
     public string Description { get; set; }
+    public string Genre { get; set; }
+    public DateTime UploadDate { get; set; }
     public TimeSpan Duration { get; set; }
     public List<string> Tags { get; set; }
-    public string Genre { get; set; }
     public int Views { get; set; }
     public int RatingCount { get; set; }
     public bool IsPublic { get; set; }  //True = 5 min preview vids, false = Subscription full length
@@ -20,9 +20,6 @@ public class Video
 
     //Think IceCube, "EXPLICIT CONTENT" sold more NWA albums. Viewers of my site are degenerates, like me, adding "WARNING: STRONG HYPNOSIS", or "WARNING: STRONG HYPNOTIC CONTENT"  adds to the experience.
     public string ContentWarning { get; set; }
-
-    public virtual ICollection<Comment> Comments { get; set; }
-
     public int Likes { get; set; }
     public int Dislikes { get; set; }
 
@@ -34,4 +31,7 @@ public class Video
             return totalVotes > 0 ? (double)Likes / totalVotes * 100 : 0;
         }
     }
+
+    public virtual ICollection<Comment> Comments { get; set; }
+
 }
