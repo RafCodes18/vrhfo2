912742eb38f250bcfe6cdbff1cd802f41fb1de37
Rafael Parra <rafael12@gmx.com>
2023-09-07 10:14:20 -0500

    Fixed data

--- VRhfo.DB/PostDeploy/DefaultData/Videos.sql
+++ VRhfo.DB/PostDeploy/DefaultData/Videos.sql
@@ -1,7 +1,14 @@
 ﻿BEGIN 
 	INSERT INTO dbo.tblVideo (Title, Studio, ThumbnailUrl, VideoUrl, Description, Genre, UploadDate, Duration, Views, RatingCount, IsPreview, IsPublic, ContentWarning, Likes, Dislikes )
-VALUES ('Title', 'Studio', 'ThumbnailUrl', 'VideoUrl', 'desc', 'genre', '2021-11-01', '01:00:00', 1000, 500, 0, 1, 'No content warning', 400, 100),
-       ('Title2', 'Studio2', 'ThumbnailUrl2', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
-       ('Title3', 'Studio3', 'ThumbnailUrl3', 'VideoUrl3', 'desc3', 'genre3', '2022-01-01', '03:00:00', 3000, 1500, 0, 1, 'No content warning', 1200, 300);
+VALUES ('Title', 'Studio', 'lana.jpg', 'VideoUrl', 'desc', 'genre', '2021-11-01', '01:00:00', 1000, 500, 0, 1, 'No content warning', 400, 100),
+       ('Title2', 'Studio2', 'vrpic.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'azz.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'reily.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'ang.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'squat.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'puss.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'squat.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title2', 'Studio2', 'puss.jpg', 'VideoUrl2', 'desc2', 'genre2', '2021-12-01', '02:00:00', 2000, 1000, 0, 1, 'No content warning', 800, 200),
+       ('Title3', 'Studio3', 'nicole.jpg', 'VideoUrl3', 'desc3', 'genre3', '2022-01-01', '03:00:00', 3000, 1500, 0, 1, 'No content warning', 1200, 300);
 
 END 
--- VRhfo.UI/Controllers/VideoController.cs
+++ VRhfo.UI/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VRhfo.BL;
 using VRhfo.BL.Models;
 using VRhfo.UI.Views.ViewModels;
 
@@ -9,103 +10,9 @@ namespace VRhfo.UI.Controllers
         // GET: VideoController
         public ActionResult Index()
         {
-            List<Video> list = new List<Video>();
-            Video video = new Video();
-            video.Description = "Description";
-            video.Title = "Lana Rhoades";
-            video.Id = 81;
-            video.VideoUrl = "ffsjof";
-            video.UploadDate = DateTime.Now;
-            video.ThumbnailUrl = "lana.jpg";
-            video.Studio = "BangBros";
-            list.Add(video);
-            Video video2 = new Video();
-            video2.Description = "Description";
-            video2.Title = "Hot Anal";
-            video2.Id = 71;
-            video2.VideoUrl = "2nd";
-            video2.UploadDate = DateTime.Now;
-            video2.ThumbnailUrl = "vrpic.jpg";
-            video2.Studio = "XXX";
-            list.Add(video2);
-            Video video3 = new Video();
-            video3.Description = "Shes on fire.";
-            video3.Title = "Nicole Aniston";
-            video3.Id = 16;
-            video3.VideoUrl = "ffsjof";
-            video3.UploadDate = DateTime.Now;
-            video3.ThumbnailUrl = "nicole.jpg";
-            video3.Studio = "Pornhub";
-            list.Add(video3);
-            Video video4 = new Video();
-            video4.Description = "Description";
-            video4.Title = "Fat ass riding";
-            video4.Id = 15;
-            video4.VideoUrl = "ffsjof";
-            video4.UploadDate = DateTime.Now;
-            video4.ThumbnailUrl = "azz.jpg";
-            video4.Studio = "Naughty America";
-            list.Add(video4);
-            Video video5 = new Video();
-            video5.Description = "Description";
-            video5.Title = "Reily Reed 3 Some";
-            video5.Id = 14;
-            video5.VideoUrl = "ffsjof";
-            video5.UploadDate = DateTime.Now;
-            video5.ThumbnailUrl = "reily.jpg";
-            video5.Studio = "BangBros";
-            list.Add(video5);
-            Video video6 = new Video();
-            video6.Description = "Description";
-            video6.Title = "Angela White TitFucki";
-            video6.Id = 11;
-            video6.VideoUrl = "ffsjof";
-            video6.UploadDate = DateTime.Now;
-            video6.ThumbnailUrl = "ang.jpg";
-            video6.Studio = "NaughtyAmerica";
-            list.Add(video6);
+            List<Video> videoList = VideoManager.LoadAll();
 
-            Video video7 = new Video();
-            video7.Description = "Description";
-            video7.Title = "Squat";
-            video7.Id = 12;
-            video7.VideoUrl = "ffsjof";
-            video7.UploadDate = DateTime.Now;
-            video7.ThumbnailUrl = "squat.jpg";
-            video7.Studio = "NaughtyAmerica";
-            list.Add(video7);
-
-            Video video8 = new Video();
-            video8.Description = "Description";
-            video8.Title = "Puss";
-            video8.Id = 21;
-            video8.VideoUrl = "ffsjof";
-            video8.UploadDate = DateTime.Now;
-            video8.ThumbnailUrl = "puss.jpg";
-            video8.Studio = "NaughtyAmerica";
-            list.Add(video8);
-
-            Video video9 = new Video();
-            video9.Description = "Description";
-            video9.Title = "Squat";
-            video9.Id = 20;
-            video9.VideoUrl = "ffsjof";
-            video9.UploadDate = DateTime.Now;
-            video9.ThumbnailUrl = "squat.jpg";
-            video9.Studio = "NaughtyAmerica";
-            list.Add(video9);
-
-            Video video10 = new Video();
-            video10.Description = "Description";
-            video10.Title = "Puss";
-            video10.Id = 10;
-            video10.VideoUrl = "ffsjof";
-            video10.UploadDate = DateTime.Now;
-            video10.ThumbnailUrl = "puss.jpg";
-            video10.Studio = "NaughtyAmerica";
-            list.Add(video10);
-
-            return View(list);
+            return View(videoList);
         }
 
         // GET: VideoController/Details/5
