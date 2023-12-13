8b9cd2db0f3d55610465888d9dd8632a313911cc
Rafael Parra <rafael12@gmx.com>
2023-12-13 17:51:30 -0600

    Working on UserController/manager

---
+++ VRhfo.BL/UserManager.cs
@@ -0,0 +1,66 @@
+﻿using VRhfo.BL.Models;
+using VRhfo.PL;
+
+namespace VRhfo.BL
+{
+    public static class UserManager
+    {
+        public static User LoadById(int id)
+        {
+            try
+            {
+                using (VRhfoEntities dc = new VRhfoEntities())
+                {
+                    tblUser tblUser = dc.tblUsers.FirstOrDefault(x => x.Id == id);
+                    User user = new User();
+                    user.Id = id;
+                    user.SubscribedDate = tblUser.SubscribedDate;
+                    user.Username = tblUser.Username;
+                    user.Email = tblUser.Email;
+                    user.Auth0UserId = tblUser.Auth0UserId;
+                    user.IsSubscribed = tblUser.IsSubscribed == 1 ? true : false;
+                    user.RegistrationDate = tblUser.RegistrationDate;
+
+                    return user;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static int Insert(User user, bool rollback = false)
+        {
+            try
+            {
+                int results = 0;
+                using (VRhfoEntities dc = new VRhfoEntities())
+                {
+                    tblUser tb = new tblUser()
+                    {
+                        Email = user.Email,
+                        Auth0UserId = user.Auth0UserId,
+                        IsSubscribed = (byte)(user.IsSubscribed == true ? 1 : 0),
+                        RegistrationDate = user.RegistrationDate,
+                        Username = user.Username,
+                        SubscribedDate = user.SubscribedDate,
+                    };
+
+                    dc.tblUsers.Add(tb);
+
+                    // Save changes to the database
+                    results = dc.SaveChanges();
+                };
+
+                return results;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ VRhfo.UI/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VRhfo.BL;
+using VRhfo.BL.Models;
+
+namespace VRhfo.UI.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult CreateAccount(User user)
+        {
+            try
+            {
+                int results = UserManager.Insert(user);
+
+                if (results != 0)
+                {
+                    Login(user);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: User/Profile
+        public ActionResult Profile(int userId)
+        {
+            User user = UserManager.LoadById(userId);
+            return View(user);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
