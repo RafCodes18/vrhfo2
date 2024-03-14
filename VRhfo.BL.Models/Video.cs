28e3642cbf666051d911d42da52dfa2c25068d4f
Rafael Parra <rafael12@gmx.com>
2024-03-13 22:10:20 -0500

    formatted video index

--- VRhfo.BL.Models/Video.cs
+++ VRhfo.BL.Models/Video.cs
@@ -17,6 +17,39 @@
         public bool IsPublic { get; set; }  //For full length vids that are free, but not previews
         public bool IsPreview { get; set; }// If True, returns 5 min preview, if false must be paid.
 
+        public string FormattedDuration
+        {
+            get
+            {
+                if (Duration.TotalHours >= 1)
+                {
+                    return $"{Duration.Hours}h {Duration.Minutes}m";
+                }
+                else
+                {
+                    return $"{Duration.Minutes}m";
+                }
+            }
+        }
+
+        public string FormattedLikes
+        {
+            get
+            {
+                if (Likes < 1000)
+                {
+                    return Likes.ToString();
+                }
+                else if (Likes < 1000000)
+                {
+                    return (Likes / 1000.0).ToString("F1") + "k";
+                }
+                else
+                {
+                    return (Likes / 1000000.0).ToString("F1") + "m";
+                }
+            }
+        }
 
         //Think IceCube, "EXPLICIT CONTENT" sold more NWA albums. Viewers of my site are degenerates, like me, adding "WARNING: STRONG HYPNOSIS", or "WARNING: STRONG HYPNOTIC CONTENT"  adds to the experience.
         public string ContentWarning { get; set; }
@@ -33,6 +66,8 @@
         }
 
         public virtual ICollection<Comment> Comments { get; set; }
+
+
 
     }
 }
