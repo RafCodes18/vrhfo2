2e45287bfe2c4c001d92f67603d7d84700afccaa
Rafael Parra <rafael12@gmx.com>
2024-03-24 14:59:31 -0500

    fixed dummy data

--- VRhfo.DB/PostDeploy/DefaultData/Comments.sql
+++ VRhfo.DB/PostDeploy/DefaultData/Comments.sql
@@ -4,5 +4,5 @@
 			('This is comment 2 which belongs to user 2', '2023-09-03', 2, 2),
 			('This is a comment 3 which belongs to user 3', '2023-09-03', 3, 1),
 			('This is a comment 4 which belongs to user 3', '2023-09-03', 3, 4),
-			('This is a comment 5 which belongs to user 1', '2023-09-03', 3, 2)
+			('This is a comment 5 which belongs to user 1', '2023-09-03', 1, 2)
 END
