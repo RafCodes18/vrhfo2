c6d667a854c8cbbccad7a78eb65ea3219983f822
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2024-05-07 22:30:27 -0500

    Added authentication to navbar and site. need to catch login errors.

---
+++ VRhfo.UI/Models/Authenticate.cs
@@ -0,0 +1,12 @@
+﻿using VRhfo.BL.Models;
+
+namespace VRhfo.UI.Models
+{
+    public class Authenticate
+    {
+        public static bool IsAuthenticated(HttpContext context)
+        {
+            return context == null ? true : context.Session.GetObject<User>("user") != null;
+        }
+    }
+}
