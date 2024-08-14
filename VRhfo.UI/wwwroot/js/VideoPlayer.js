964ede73e8f4ae5963d20a5abb466f80a04141cd
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2024-08-13 20:16:54 -0500

    one stop shop for porn obsessed degenerates. new video player in progress!!

---
+++ VRhfo.UI/wwwroot/js/VideoPlayer.js
@@ -0,0 +1,40 @@
+﻿const playPauseBtn = document.querySelector(".play-pause-btn");
+const video = document.querySelector('video');
+const videoContainer = document.querySelector(".video-container");
+
+
+document.addEventListener("keydown", e => {
+        //STOP SCROLL ON SPACEBAR
+        if (e.keyCode == 32 && e.target == document.body) {
+            e.preventDefault();
+        }
+   
+    switch (e.key.toLowerCase()) {
+        case "k":
+        case " ":
+            togglePlay()
+            break;
+    }
+});
+
+playPauseBtn.addEventListener('click', () => {
+    togglePlay();
+});
+video.addEventListener('click', () => {
+    togglePlay();
+    console.log("video clicked");
+});
+
+
+video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused");
+});
+
+video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused");
+});
+
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
