18b200a79b6286838e81b26089848de014f06e92
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2024-09-07 03:22:27 -0500

    shame on yu

--- VRhfo.BL.Models/Comment.cs
+++ VRhfo.BL.Models/Comment.cs
@@ -12,6 +12,47 @@
         //navigation properties (not database fields)
         public User User { get; set; }
         public Video Video { get; set; }
+
+
+
+        public string FormattedUploadDate
+        {
+            get
+            {
+                var currentDate = DateTime.UtcNow;
+                var dateDifference = currentDate - DatePosted;
+
+                if (dateDifference.TotalDays < 1)
+                {
+                    return "Today";
+                }
+                else if (dateDifference.TotalDays < 2)
+                {
+                    return "1 day ago";
+                }
+                else if (dateDifference.TotalDays < 7)
+                {
+                    return $"{(int)dateDifference.TotalDays} days ago";
+                }
+                else if (dateDifference.TotalDays < 14)
+                {
+                    return "1 wk ago";
+                }
+                else if (dateDifference.TotalDays < 30)
+                {
+                    return $"{(int)(dateDifference.TotalDays / 7)} wks ago";
+                }
+                else if (dateDifference.TotalDays < 365)
+                {
+                    return $"{(int)(dateDifference.TotalDays / 30)} mths ago";
+                }
+                else
+                {
+                    return $"{(int)(dateDifference.TotalDays / 365)} yrs ago";
+                }
+            }
+        }
+
     }
 
 }
