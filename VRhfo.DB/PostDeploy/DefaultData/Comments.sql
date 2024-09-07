ecd0e2e7f7e940805ee921337fe4f1106dd618bf
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2024-09-07 02:33:15 -0500

    when user is typing a comment need to disable key triggers for fullscreen, mute, theater, etc

--- VRhfo.DB/PostDeploy/DefaultData/Comments.sql
+++ VRhfo.DB/PostDeploy/DefaultData/Comments.sql
@@ -1,16 +1,17 @@
-﻿BEGIN 
-	INSERT INTO tblComment( Content, DatePosted, UserId, VideoId)
-	VALUES('This is comment 1 which belongs to user 1', '2023-09-03', 1, 3),
-			('This is comment 2 which belongs to user 2', '2023-09-03', 2, 2),
-			('This is a comment 3 which belongs to user 3', '2023-09-03', 3, 1),
-			('This is a comment 4 which belongs to user 3', '2023-09-03', 3, 4),
-			('This is a comment 5 which belongs to user 1', '2023-09-03', 1, 2),
-			('No way', '2023-09-03', 3, 2),
-			('Amazinggg', '2023-09-03', 3, 1),
-			('omg', '2023-09-03', 1, 8),
-			('so fucking hot', '2023-09-03', 1, 10),
-			('yesss', '2023-09-03', 2, 9),
-			('wow', '2023-09-03', 3, 9),
-			('awesome', '2023-09-03', 3, 1),
-			('Lmao', '2023-09-03', 1, 1)
+﻿BEGIN
+    INSERT INTO tblComment(Content, DatePosted, UserId, VideoId)
+    VALUES
+        ('This is comment 1 which belongs to usefr 1', '2023-09-03', 1, 3),
+        ('This is comment 2 which belongs to usefr 2', '2023-09-03', 2, 2),
+        ('This is a comment 3 which belongs to user 3', '2023-09-03', 3, 1),
+        ('This is a comment 4 which belongs to usefr 3', '2023-09-03', 3, 1),
+        ('This is a comment 5 which belongs to user 1', '2023-09-03', 1, 1),
+        ('No wfay', '2023-09-03', 3, 2),
+        ('Amazinfggg', '2023-09-03', 3, 1),
+        ('omg', '2023-09-03', 1, 8),
+        ('so fucking hoft', '2023-09-03', 1, 1),
+        ('yefsss', '2023-09-03', 2, 1),
+        ('wow', '2023-09-03', 3, 1),
+        ('awfesome', '2023-09-03', 3, 1),
+        ('Lmafo', '2023-09-03', 1, 1);
 END
