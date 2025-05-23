519f6b359947e31bfb0eb8772ad607c9e54d8d64
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2025-05-23 11:12:23 -0500

    Found double scroll error (media query mainpg height set to 160 instead of adding padding to cards to overcome mobile footer)

--- VRhfo.UI/wwwroot/js/site.js
+++ VRhfo.UI/wwwroot/js/site.js
@@ -1,8 +1,13 @@
 ﻿const searchCue = document.querySelector('.btn-outline-primary');
 const searchBar = document.querySelector('.custom-search');
+const mobileSearchBar = document.querySelector("mobile-search-bar");
 
 const searchInput = document.querySelector("[data-search]");
+const mosearchInput = document.querySelector("[data-search]");
+
 const resultBox = document.querySelector(".result-box");
+const resultBox2 = document.querySelector(".result-box-2");
+
 
 let suggestions = [
     'Hypnosis porn trance',
@@ -64,3 +69,24 @@ searchInput.addEventListener("input", e => {
         resultBox.innerHTML = '';
     }
 });
+
+
+mobileSearchBar.addEventListener("focus", () => {
+    searchCue.classList.add("search-cue");
+});
+mobileSearchBar.addEventListener("focusout", () => {
+    searchCue.classList.remove("search-cue");
+});
+
+mosearchInput.addEventListener("input", e => {
+    const value = e.target.value;
+    console.log(value);
+    result = suggestions.filter((keyword) => {
+        return keyword.toLowerCase().includes(value.toLowerCase());
+    });
+    console.log(result);
+    display(result);
+    if (!result.length) {
+        resultBox2.innerHTML = '';
+    }
+});s
