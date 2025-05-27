d4744f5261012336b205e95eb9c0ee7bab644cd4
Rafael Parra <105762046+RafCodes18@users.noreply.github.com>
2025-05-27 16:32:15 -0500

    Added email service, can send emails with google account

---
+++ EmailService/Controllers/EmailsController.cs
@@ -0,0 +1,23 @@
+﻿using EmailService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Controllers
+{
+    [Route("api/emails")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        public EmailsController(IEmailService emailService) 
+        { 
+                this._emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(string reciever, string subject, string body)
+        {
+            await _emailService.SendEmail(reciever, subject, body);
+            return Ok();
+        }
+    }
+}
